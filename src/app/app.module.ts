88f0a492e9890e80450cc36a5c91814bf0d975b4
AugustoSchlickmann <augusto3625@hotmail.com>
2019-05-16 20:00:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
+import { EstadosPesquisaComponent } from './estados/estados-pesquisa/estados-pesquisa.component';
+import { EstadosCadastroComponent } from './estados/estados-cadastro/estados-cadastro.component';
+import { CategoriasCadastroComponent } from './categorias/categorias-cadastro/categorias-cadastro.component';
+import { CategoriasPesquisaComponent } from './categorias/categorias-pesquisa/categorias-pesquisa.component';
+import { CategoriasModule } from './categorias/categorias.module';
+import { EstadosModule } from './estados/estados.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ConfirmationService, MenuItem } from 'primeng/api';
+
+import {Routes, RouterModule} from '@angular/router';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
+import {TableModule} from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+
+
+
+const rotas: Routes = [
+  {path: 'categorias', component: CategoriasPesquisaComponent},
+  {path: 'categorias/novo', component: CategoriasCadastroComponent},
+  {path: 'categorias/:id', component: CategoriasCadastroComponent},
+  {path: 'estados', component: EstadosPesquisaComponent},
+  {path: 'estados/novo', component: EstadosCadastroComponent},
+  {path: 'estados/:id', component: EstadosCadastroComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CategoriasModule,
+    EstadosModule,
+    HttpClientModule,
+    RouterModule.forRoot(rotas),
+    InputTextModule,
+    ButtonModule,
+    TooltipModule,
+    TableModule,
+    FormsModule,
+    ConfirmDialogModule,
+    RouterModule,
+
   ],
-  providers: [],
+  providers: [
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
